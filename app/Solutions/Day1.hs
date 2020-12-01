56397ecc177ecacc0992972121054993b37e24e5
Thomas Li <guslithd@student.gu.se>
2020-12-01 21:26:33 +0100

    Day 1: use existing function `rights`

--- app/Solutions/Day1.hs
+++ app/Solutions/Day1.hs
@@ -5,20 +5,15 @@ module Solutions.Day1
 
 import Data.Text.Read (decimal)
 
+
 solve :: Text -> Maybe Integer
 solve =
     fmap (\ (a, b, c) -> a * b * c)
     . findSum
-    . getRights
+    . rights
     . map (second fst . decimal)
     . words
   where
-    getRights :: [Either a b] -> [b]
-    getRights = \case
-        []           -> []
-        Left _ : xs  -> getRights xs
-        Right b : xs -> b : getRights xs
-
     findSum :: [Integer] -> Maybe (Integer, Integer, Integer)
     findSum ints = listToMaybe
         [ (a, b, c)
