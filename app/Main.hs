8977a13b87eeca11492d90a8d0e4be9eff1359e1
Thomas Li <guslithd@student.gu.se>
2020-12-04 03:59:59 +0100

    Add support for specifying Part

--- app/Main.hs
+++ app/Main.hs
@@ -19,7 +19,8 @@ main :: IO ()
 main = do
     args <- parseArgsIO ArgsComplete [argumentDay, argumentInputFile]
     let day :: Int = getRequiredArg args 1
-    let inputFile :: String = getRequiredArg args 2
+    let part :: Int = getRequiredArg args 2
+    let inputFile :: String = getRequiredArg args 3
     case day of
         1 -> D1.solveIO inputFile
         2 -> D2.solveIO inputFile
@@ -35,9 +36,18 @@ main = do
         , argDesc = "Day of solution (1-25)."
         }
 
+    argumentPart :: Arg Int
+    argumentPart = Arg
+        { argIndex = 2
+        , argAbbr = Just 'p'
+        , argName = Just "part"
+        , argData = argDataRequired "PART" ArgtypeInt
+        , argDesc = "Part of solution (1 or 2)"
+        }
+
     argumentInputFile :: Arg Int
     argumentInputFile = Arg
-        { argIndex = 2
+        { argIndex = 3
         , argAbbr = Just 'i'
         , argName = Just "input"
         , argData = argDataRequired "FILE" ArgtypeString
