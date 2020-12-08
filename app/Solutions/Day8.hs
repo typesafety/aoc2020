64cb9b8a5957704877c0f7cbbfded4213d7da7f5
Thomas Li <guslithd@student.gu.se>
2020-12-08 23:17:00 +0100

    D8: part 2 done

--- app/Solutions/Day8.hs
+++ app/Solutions/Day8.hs
@@ -8,8 +8,7 @@ module Solutions.Day8
 import Data.Text.Read (decimal, signed)
 import Lens.Micro.Platform (modifying, use, view, makeLenses)
 import Relude.Extra (bimapBoth)
-
-import Misc.Misc (if')
+import qualified Relude.Unsafe as Unsafe
 
 import qualified Data.Text as T
 import qualified Data.IntMap.Strict as IM
@@ -31,40 +30,67 @@ data St = St
     { _stAcc     :: Int
     , _stPc      :: Int
     , _stVisited :: Set Int
-    }
-
-type RunProg = ReaderT Prog (State St)
+    } deriving (Show, Read, Ord, Eq)
 
 $(makeLenses ''St)
 
+initialState :: St
+initialState = St
+    { _stAcc = 0
+    , _stPc = 0
+    , _stVisited = S.empty
+    }
+
+type Execute = ReaderT Prog (State St)
+
 solve1 :: Text -> Int
-solve1 = maybe (error "💩") (view stAcc . runProg) . pProg
+solve1 = view stAcc . snd . runProg . Unsafe.fromJust . pProg
 
 solve2 :: Text -> Int
-solve2 = error "Not yet solved"
+solve2 =
+    view stAcc
+    . Unsafe.head
+    . mapMaybe (fst . runProg)
+    . mutations
+    . Unsafe.fromJust
+    . pProg
 
-runProg :: Prog -> St
-runProg = flip execState initialState . runReaderT execute
+mutations :: Prog -> [Prog]
+mutations prog = map (mutate prog) . S.toList . view stVisited . snd . runProg $ prog
   where
-    initialState :: St
-    initialState = St
-        { _stAcc = 0
-        , _stPc = 0
-        , _stVisited = S.empty
-        }
+    mutate :: Prog -> Int -> Prog
+    mutate p idx = IM.adjust (\ (Instr opr arg) -> Instr (tl opr) arg) idx p
+      where
+        tl :: Opr -> Opr
+        tl = \case
+            Jmp -> Nop
+            Nop -> Jmp
+            x   -> x
 
-execute :: RunProg ()
-execute =
-    (,) <$> use stPc <*> use stVisited
-    >>= flip (`if'` pass) (step >> execute) . uncurry S.member  -- lololol
+runProg :: Prog -> (Maybe St, St)
+runProg prog = runState (runReaderT execute prog) initialState
   where
-    step :: RunProg ()
+    execute :: Execute (Maybe St)
+    execute =
+        finished >>= \case
+            True  -> Just <$> get
+            False -> stuck >>= \case
+                True  -> pure Nothing
+                False -> step >> execute
+      where
+        finished :: Execute Bool
+        finished = not <$> (IM.member <$> use stPc <*> ask)
+
+        stuck :: Execute Bool
+        stuck = S.member <$> gets _stPc <*> gets _stVisited
+
+    step :: Execute ()
     step = do
         pc <- use stPc
         modifying stVisited $ S.insert pc
         ask >>= runInstr . (IM.! pc)
       where
-        runInstr :: Instr -> RunProg ()
+        runInstr :: Instr -> Execute ()
         runInstr (Instr i (Arg n)) = do
             stAcc `modifying` case i of
                 Acc -> (+ n)
