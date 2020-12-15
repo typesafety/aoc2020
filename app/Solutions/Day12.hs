30b1e46e92c3f1ade646ae9b011259bf12566b0f
Thomas Li <guslithd@student.gu.se>
2020-12-15 19:20:48 +0100

    D12: part 2 done

--- app/Solutions/Day12.hs
+++ app/Solutions/Day12.hs
@@ -8,6 +8,8 @@ import Relude.Extra (bimapBoth)
 import qualified Data.Text as T
 import qualified Relude.Unsafe as Unsafe
 
+import qualified Misc.Misc as Misc
+
 
 newtype Angle = Angle Int
     deriving (Show)
@@ -47,6 +49,7 @@ data Instr
     deriving (Show)
 
 type Point = (Int, Int)
+type Waypoint = Point
 
 solve1 :: Text -> Int
 solve1 =
@@ -58,29 +61,57 @@ solve1 =
     . lines
 
 solve2 :: Text -> Int
-solve2 = error "Not yet solved"
+solve2 =
+    uncurry (+)
+    . bimapBoth abs
+    . flip evalState (10, 1)
+    . foldlM (flip step2) (0, 0)
+    . map pAction
+    . lines
+
+rotate :: Int -> Waypoint -> Waypoint
+rotate amt oldWaypoint = newWaypoint
+  where
+    numSteps :: Int
+    numSteps = abs amt `mod` 360 `div` 90
+
+    newWaypoint :: Waypoint
+    newWaypoint = Misc.applyN numSteps oneStep oldWaypoint
+
+    oneStep :: Waypoint -> Waypoint
+    oneStep (wX, wY) = if amt > 0 then (-wY, wX) else (wY, -wX)
+
+step2 :: Instr -> Point -> State Waypoint Point
+step2 (Turn tDir (Angle n)) p = modify' (rotate signedAngle) >> pure p
+  where
+    signedAngle :: Int
+    signedAngle = case tDir of
+        L -> n
+        R -> (-n)
+step2 (Move dir (Distance n)) p = case dir of
+    DirCardinal cDir -> modify' (mv cDir n) >> pure p
+    DirForward -> foldl' (\ (wX, wY) -> bimap (+ wX) (+ wY)) p . replicate n <$> get
 
 step :: Instr -> Point -> State CardinalDir Point
-step (Turn tDir (Angle n))   p = do
+step (Turn tDir (Angle n)) p = do
     let angle = case tDir of
             L -> n
             R -> (-n)
-    modify' (`turn` angle)
+    modify' (turn angle)
     pure p
-  where
-    turn :: CardinalDir -> Int -> CardinalDir
-    turn cd amt = toCardinalDir $ (toDegrees cd + amt) `mod` 360
-
 step (Move dir (Distance n)) p = case dir of
     DirCardinal cDir -> pure $ mv cDir n p
     DirForward       -> (\ cDir -> mv cDir n p) <$> get
-  where
-    mv :: CardinalDir -> Int -> Point -> Point
-    mv direction amt (oldX, oldY) = case direction of
-        N -> (oldX, oldY + amt)
-        E -> (oldX + amt, oldY)
-        S -> (oldX, oldY - amt)
-        W -> (oldX - amt, oldY)
+
+turn :: Int -> CardinalDir -> CardinalDir
+turn amt cd = toCardinalDir $ (toDegrees cd + amt) `mod` 360
+
+mv :: CardinalDir -> Int -> Point -> Point
+mv direction amt (oldX, oldY) = case direction of
+    N -> (oldX, oldY + amt)
+    E -> (oldX + amt, oldY)
+    S -> (oldX, oldY - amt)
+    W -> (oldX - amt, oldY)
 
 pAction :: Text -> Instr
 pAction t =
