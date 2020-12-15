1755a1cf702074079c6187fa7e167d143c6e2d86
Thomas Li <guslithd@student.gu.se>
2020-12-15 19:32:06 +0100

    D12: Clean up part 1 and 2 solutions

--- app/Solutions/Day12.hs
+++ app/Solutions/Day12.hs
@@ -14,6 +14,11 @@ import qualified Misc.Misc as Misc
 newtype Angle = Angle Int
     deriving (Show)
 
+signAngle :: TurnDir -> Angle -> Angle
+signAngle tDir (Angle n) = case tDir of
+    L -> Angle n
+    R -> Angle (-n)
+
 newtype Distance = Distance Int
     deriving (Show)
 
@@ -69,43 +74,37 @@ solve2 =
     . map pAction
     . lines
 
-rotate :: Int -> Waypoint -> Waypoint
-rotate amt oldWaypoint = newWaypoint
-  where
-    numSteps :: Int
-    numSteps = abs amt `mod` 360 `div` 90
-
-    newWaypoint :: Waypoint
-    newWaypoint = Misc.applyN numSteps oneStep oldWaypoint
-
-    oneStep :: Waypoint -> Waypoint
-    oneStep (wX, wY) = if amt > 0 then (-wY, wX) else (wY, -wX)
-
 step2 :: Instr -> Point -> State Waypoint Point
-step2 (Turn tDir (Angle n)) p = modify' (rotate signedAngle) >> pure p
-  where
-    signedAngle :: Int
-    signedAngle = case tDir of
-        L -> n
-        R -> (-n)
+step2 (Turn tDir angle) p = modify' (rotate (signAngle tDir angle)) >> pure p
 step2 (Move dir (Distance n)) p = case dir of
     DirCardinal cDir -> modify' (mv cDir n) >> pure p
     DirForward -> foldl' (\ (wX, wY) -> bimap (+ wX) (+ wY)) p . replicate n <$> get
 
+{- | Rotate the given waypoint around the origin (0, 0) by the given amount,
+in steps of 90 degrees.
+-}
+rotate :: Angle -> Waypoint -> Waypoint
+rotate (Angle amt) = Misc.applyN numSteps oneStep
+  where
+    numSteps :: Int
+    numSteps = abs amt `mod` 360 `div` 90
+
+    oneStep :: Waypoint -> Waypoint
+    oneStep (wX, wY) = if amt > 0 then (-wY, wX) else (wY, -wX)
+
 step :: Instr -> Point -> State CardinalDir Point
-step (Turn tDir (Angle n)) p = do
-    let angle = case tDir of
-            L -> n
-            R -> (-n)
-    modify' (turn angle)
-    pure p
+step (Turn tDir angle) p = modify' (turn (signAngle tDir angle)) >> pure p
 step (Move dir (Distance n)) p = case dir of
     DirCardinal cDir -> pure $ mv cDir n p
     DirForward       -> (\ cDir -> mv cDir n p) <$> get
 
-turn :: Int -> CardinalDir -> CardinalDir
-turn amt cd = toCardinalDir $ (toDegrees cd + amt) `mod` 360
+{- | Calculate a new direction given a starting direction and the amount to
+turn in degrees (in steps of 90 degrees).
+-}
+turn :: Angle -> CardinalDir -> CardinalDir
+turn (Angle amt) cd = toCardinalDir $ (toDegrees cd + amt) `mod` 360
 
+-- | Move a point @n@ units in the given direction.
 mv :: CardinalDir -> Int -> Point -> Point
 mv direction amt (oldX, oldY) = case direction of
     N -> (oldX, oldY + amt)
@@ -113,6 +112,7 @@ mv direction amt (oldX, oldY) = case direction of
     S -> (oldX, oldY - amt)
     W -> (oldX - amt, oldY)
 
+-- | Parse a text string into an instruction.
 pAction :: Text -> Instr
 pAction t =
     let (x, nn) :: (Text, Int) = second (Unsafe.read . toString) $ T.splitAt 1 t
